9dc1d3a771b7f2bd26afd4e7373ec8c9270a84ca
omotosho toheeb <omotoshotoheeb2022@gmail.com>
2026-06-25 07:21:57 +0100

    token update

--- kaykes/lib/axiosClient.ts
+++ kaykes/lib/axiosClient.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export const axiosClient = axios.create({
     baseURL: 'https://ingenious-horse-4f1726e4c5.strapiapp.com/api',
     headers: {
-        Authorization: `Bearer ` + process.env.STRAPI_API_TOKEN
+        Authorization: `Bearer ` + process.env.NEXT_PUBLIC_STRAPI_API_TOKEN
     }
 })
